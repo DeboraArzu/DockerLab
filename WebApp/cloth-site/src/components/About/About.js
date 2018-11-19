ea99a4cd9e148d3979ef37265a3c2c7e18a05bc0
DeboraArzu <deboranise29@hotmail.com>
2018-11-18 22:30:14 -0600

    changes in About and dockerfile server

--- WebApp/cloth-site/src/components/About/About.js
+++ WebApp/cloth-site/src/components/About/About.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './About.css'
+import { Well, Grid, Row, Col } from 'react-bootstrap'
+import Logo from '../img/logo.png'
 
 class About extends Component {
-    render(){
-        return(
-            <div className="content" >
-                <h1>This page was Made By Debora Arzu</h1>
-                <h2>1098913</h2>
+    render() {
+        return (
+            <div>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={6} md={4}>
+                            <div className="content">
+                                <img height={110} width={80} src={Logo}></img>
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
+                <div className="content" >
+                    <Well className="Well">
+                        <h1>This page was Made By Debora Arzu</h1>
+                        <h2>Carné 1098913</h2>
+                    </Well>
+                </div>
             </div>
         )
     }
