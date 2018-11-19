6b002023fd5d4956738800c874f16cae68a79b60
DeboraArzu <deboranise29@hotmail.com>
2018-11-19 00:37:16 -0600

    product controller updated

--- webapp/server/controllers/product.controller.js
+++ webapp/server/controllers/product.controller.js
@@ -17,6 +17,7 @@ exports.getproducts = function (req, res, next) {
 
 //HTTP POST
 exports.product_create = function (req, res, next) {
+
     let product = new Product(
         {
             name: req.body.name,
@@ -27,14 +28,14 @@ exports.product_create = function (req, res, next) {
             codigobarra: req.body.codigobarra
         }
     );
+    insert(req.params.codigobarra, product)
     //Save the information to the DB
     product.save(function (err) {
         if (err) {
             return next(err);
         }
         res.status(200).send('Product Created successfully')
-        insert(req.body.codigobarra, product)
-    }).catch(next);
+    })
 
 };
 
@@ -50,7 +51,7 @@ exports.product_details = function (req, res) {
 
 //HTTP PUT
 exports.product_update = function (req, res, next) {
-   Product.findOneAndUpdate(req.params.codigobarra, { $set: req.body }, function (err, product) {
+    Product.findOneAndUpdate(req.params.codigobarra, { $set: req.body }, function (err, product) {
         if (err) return next(err);
         res.status(200).send('Product udpated.');
     }).catch(next);
@@ -58,7 +59,7 @@ exports.product_update = function (req, res, next) {
 
 //DELETE
 exports.product_delete = function (req, res, next) {
-    Product.findOneAndRemove(req.params.codigobarra, function (err) {
+    Product.findOneAndDelete(req.params.codigobarra, function (err) {
         if (err) return next(err);
         res.status(200).send('Deleted successfully!');
         deletedata(req.params.codigobarra)
@@ -79,23 +80,23 @@ function deletedata(codigobarra) {
     client.del(codigobarra);
 }
 
-function GetKeys(){
+function GetKeys() {
     var products = [];
     client.keys('*', function (err, keys) {
         if (err) return console.log(err);
-        if(keys){
-            async.map(keys, function(key, cb) {
-               client.get(key, function (error, value) {
+        if (keys) {
+            async.map(keys, function (key, cb) {
+                client.get(key, function (error, value) {
                     if (error) return cb(error);
                     var product = {};
-                    product['codigobarra']=key;
-                    product['data']=value;
+                    product['codigobarra'] = key;
+                    product['data'] = value;
                     cb(null, product);
-                }); 
+                });
             }, function (error, results) {
-               if (error) return console.log(error);
-               console.log(results);
-               res.json({data:results});
+                if (error) return console.log(error);
+                console.log(results);
+                res.json({ data: results });
             });
         }
     });
